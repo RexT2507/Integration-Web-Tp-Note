bc494a57be9a12f9d3c14c707527296955e3bd32
RexT2507 <baptiste.marmierskyrim@hotmail.fr>
2021-04-16 16:06:28 +0200

    [FIX] Code Review

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Integration-Web-Tp-Note');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Integration-Web-Tp-Note app is running!');
-  });
 });
