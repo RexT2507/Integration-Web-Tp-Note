28e6709223255d18664c43d874b0028c6d5790bb
RexT2507 <baptiste.marmierskyrim@hotmail.fr>
2021-04-26 00:21:11 +0200

    [DEV][ADD] Mise en place de la page fruit + collapse menu

--- src/app/fruit-page/fruit-page.component.ts
+++ src/app/fruit-page/fruit-page.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-fruit-page',
   templateUrl: './fruit-page.component.html',
-  styleUrls: ['./fruit-page.component.scss']
+  styleUrls: ['./fruit-page.component.scss'],
+  animations: [
+		trigger('flyInOut', [
+			// state('*', style({opacity: 1, transform: 'translateX(0)'})),
+			transition('void => *', [
+				style({
+					opacity: 0,
+					transform: 'translateY(-100%)'
+				}),
+				animate('0.8s ease-out')
+			]),
+			transition('* => void', [
+				animate('0.8s ease-out', style({
+					opacity: 0,
+					transform: 'translateY(100%)'
+				}))
+			])
+		])
+	]
+
 })
 export class FruitPageComponent implements OnInit {
+
+  @HostBinding('@flyInOut')
+
+
+	active: boolean = false;
 
   constructor() { }
 
